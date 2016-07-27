360c38feaa8d26aeb74179f8748eea7389baf6af
monslow <mikeo@clarityvoice.com>
2016-07-27 07:31:34 -0400

    Boilerplate base

---
+++ Main.elm
@@ -0,0 +1,71 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+
+{-- Remove commented imports as needed --}
+
+-- import Json.Decode as Json
+-- import Json.Decode.Pipeline exposing (decode, required, optional)
+-- import Task
+
+
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+-- MODEL
+
+
+type alias Model = { props : List String }
+
+
+init : (Model, Cmd Msg)
+init =
+  ( Model []
+    , Cmd.none
+   )
+
+
+-- UPDATE
+
+
+type Msg
+  = FetchSucceed Model
+  | FetchFail Http.Error
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+
+    FetchSucceed model ->
+      (model, Cmd.none)
+
+    FetchFail _ ->
+      (model, Cmd.none)
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div [ ] [ text "Hello Boilerplate" ]
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
